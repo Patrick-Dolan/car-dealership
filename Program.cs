07a1c7488986f9d21ab71371098ef2ad03ebc5a2
Patrick Dolan <dolanp1992@gmail.com>
2022-02-14 18:59:52 -0800

    start project
    Co-authored-by: Anastasia Han <anastasia.han@yahoo.com>

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Dealership.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Dealership
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
+  }
+}
